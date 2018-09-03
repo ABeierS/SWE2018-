f49821568bb036ea6556f6573b9afc5584719f0c
ABEeierS <andreas.beier@live.dk>
2018-09-03 14:00:51 +0200

    Working state, simpel

--- rle.py
+++ rle.py
@@ -1,18 +1,21 @@
 
 
 def rle_encoder(txt):
-    """ RLE encoder modetager en streng og returner en streng
-        med så gentagne bogstaver bilver erstattet med bogstavet og
-         antallet af gentagelser
+	""" RLE encoder modetager en streng og returner en streng
+		med så gentagne bogstaver bilver erstattet med bogstavet og
+		antallet af gentagelser
 
-         "bbbkkk" bliver "b3k3"
-         """
-    c = txt[0]
-    i = 1
-    res = []
-    for x in txt[1:]:
-        if x == c:
-            i += 1
-        else:
-            res.append(f'{c}{i}')
-    return ''.join(res)
+        "bbbkkk" bliver "b3k3"
+		"""
+	c = txt[0]
+	i = 1
+	res = []
+	for x in txt[1:]:
+		if x == c:
+			i += 1
+		else:
+			res.append(f'{c}{i}')
+			c = x
+			i = 1
+	res.append(f'{c}{i}')
+	return ''.join(res)
--- test_rle.py
+++ test_rle.py
@@ -1,6 +1,10 @@
 from rle import rle_encoder
 
 def test_simple():
-    assert rle_encoder("bbbkkk") == "b3k3"
+    assert rle_encoder("bbbkkka") == "b3k3a1"
 def test_advanced():
-    assert rle_encoder("ffffiiiii") == "f4i5"
+    assert rle_encoder("ffffiiiiiaaa") == "f4i5a3"
+def test_numbers():
+    assert rle_encoder("11777599") == "12735192"
+def test_moreNumbers():
+    assert rle_encoder("1111111111111166") == "11462"
